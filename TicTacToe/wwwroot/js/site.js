50c158375f7a58acd4750d2a52dc2a8e40a7d54b
donovanwest <donovanwest@F1092-10-2098>
2019-11-11 15:11:38 -0600

    :)

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -1,4 +1,59 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var turn = "x";
 
-// Write your Javascript code.
+
+function setTurn() {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "It is player " + turn + "'s turn";
+}
+
+function setWinner(winner) {
+    var turnElement = document.getElementById("turn");
+    turnElement.innerText = "Player " + winner.innerText + " Wins!";
+}
+
+function checkForWin() {
+    var cells = document.getElementsByClassName("square");
+    for (var i = 0; i < 9; i += 3) {
+        if (cells[i].innerText != "" && cells[0 + i].innerText === cells[1 + i].innerText && cells[1 + i].innerText == cells[2 + i].innerText) {
+            setWinner(cells[0 + 1]);
+            return true;
+        }
+    }
+
+    for (var j = 0; j < 9; j++) {
+        if (cells[j].innerText != "" && cells[0+j].innerText === cells[3+j].innerText && cells[3+j].innerText === cells[6+j].innerText) {
+            setWinner(cells[0 + j].innerText)
+            return true;
+        }
+    }
+
+    if (cells[0].innerText != "" && cells[0].innerText == cells[4].innerText && cells[4].innerText == cells[8].innerText) {
+        setWinner(cells[0]);
+        return true;
+    }
+
+    if (cells[2].innerText != "" && cells[0].innerText == cells[2].innerText && cells[4].innerText == cells[6].innerText) {
+        setWinner(cells[0]);
+        return true;
+    }
+
+    return false;
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = turn;
+        if (turn === "x") turn = "o";
+        else turn = "x";
+        setTurn();
+    }
+    if (!checkForWin())
+        setTurn();
+}
+
+var cells = document.getElementsByClassName("square");
+for (i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", onClick);
+}
+setTurn();
